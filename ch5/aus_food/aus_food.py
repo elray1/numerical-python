bcd523947dd36841a124a19bd107153ec6dfc01c
Evan Ray <eray@mtholyoke.edu>
2019-07-22 15:03:28 -0400

    update evan's solutions

--- ch5/aus_food/aus_food.py
+++ ch5/aus_food/aus_food.py
@@ -10,32 +10,55 @@ import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy.linalg as la
 
-# Problem Introduction
-# The csv file aus_food.csv contains records of total monthly expenditure on
-# cafes, restaurants and takeaway food services in Australia ($billion) from
-# 2004 through 2016. (Hyndman, R.J., & Athanasopoulos, G. (2018)
-# Forecasting: principles and practice, 2nd edition,
-# OTexts: Melbourne, Australia. OTexts.com/fpp2. Accessed on July 22, 2019.)
-
-# The file contains two variables: `expenditure` is the monthly expenditure
-# on food services, and `time` encodes the year and month. For example,
-# January 2004 is coded as 2004, and February 2004 is encoded as 2004.0833...
-# since 1/12 is about 0.083.
-# In this example we will build a model to forecast monthly food expenditures.
-
-# 1. Read the data in.  Make a plot that has food expenditures on the vertical
-# axis and time on the horizontal axis.
+# 1.
 aus_food = pd.read_csv("aus_food.csv")
 plt.plot(aus_food['time'], aus_food['expenditure'])
 
-# 2. Your plot in part 1 showed an increasing trend in food expenditures over
-# time, with fairly consistent seasonal patterns (for instance, there is a
-# regular peak in food expenditures in December of each year).  Let's use a
-# model for food expenditures that has a linear function of time to capture
-# the long-term increasing trend, and sine and cosine terms to capture
-# seasonality around that trend. Specifically, if we use y_t to denote food
-# expenditures at time t, we have:
-# y_t \approx \alpha_0 + \alpha_1 t + \sum_{k = 1}^K \{\gamma_{k} sin(2 \pi k / 12) + \gamma^*_{k} cos(2 \pi k / 12) \}
-# We can express this model in matrix form as y \approx X \beta, where
-# y = \begin{bmatrix} y_1 \\ \vdots \\ y_t
+# 2.
+t_vals = aus_food['time'].values
+t_vals = t_vals[t_vals < 2016]
+
+X = np.empty((len(t_vals), 14))
+
+X[:, 0] = 1.0
+X[:, 1] = t_vals
+
+for k in range(6):
+   X[:, 2 * (k + 1)] = np.sin(2 * np.pi * (k + 1) / 1 * t_vals)
+   X[:, 2 * (k + 1) + 1] = np.cos(2 * np.pi * (k + 1) / 1 * t_vals)
+
+fig, ax = plt.subplots()
+ax.plot(t_vals, X[:, 2], color = "red", label = "line 1")
+ax.plot(t_vals, X[:, 3], color = "red", label = "line 1")
+ax.plot(t_vals, X[:, 4], color = "blue", label = "line 1")
+ax.plot(t_vals, X[:, 5], color = "blue", label = "line 1")
+
+# 3.
+y = aus_food['expenditure'].values[range(len(t_vals))]
+sol, r, rank, sv = la.lstsq(X, y)
+
+# 4.
+t_vals_new = aus_food['time'].values
+t_vals_new = t_vals_new[t_vals_new >= 2016]
+
+X_new = np.empty((len(t_vals_new), 14))
+
+X_new[:, 0] = 1.0
+X_new[:, 1] = t_vals_new
+
+for k in range(6):
+   X_new[:, 2 * (k + 1)] = np.sin(2 * np.pi * (k + 1) / 1 * t_vals_new)
+   X_new[:, 2 * (k + 1) + 1] = np.cos(2 * np.pi * (k + 1) / 1 * t_vals_new)
+
+# 5.
+y_hat_new = np.matmul(X_new, sol)
+
+# 6.
+y_obs = aus_food['expenditure'].values[range(len(t_vals))]
+
+fig, ax = plt.subplots()
+ax.plot(aus_food['time'], aus_food['expenditure'], label = "Observed Expenditure")
+ax.plot(t_vals_new, y_hat_new, color = "orange", ls = "dashed", label = "Predicted Expenditure")
+
